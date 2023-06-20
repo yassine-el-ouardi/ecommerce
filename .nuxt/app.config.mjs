2766a3eb5299cc771c9b3441760e35451679a75f
yassine-el-ouardi <yassine.el-ouardi@edu.uiz.ac.ma>
2023-06-20 15:33:53 +0000

    after some folder placements

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/czeroo/Desktop/laravel/camacho/camacho/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
