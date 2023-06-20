f960819d09c54b33dfc4a32b3ad2bd8da54c4fa5
yassine-el-ouardi <yassine.el-ouardi@edu.uiz.ac.ma>
2023-06-20 14:49:10 +0000

    moving files and running the web app

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/Users/czeroo/Desktop/laravel/camacho/camacho/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
